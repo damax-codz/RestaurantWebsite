c295ae2350faea417fa0cc89cfb59fcdae541cee
Boluwaduro00 <adebajobolu@gmail.com>
2022-04-25 17:24:55 +0100

    worked on the about page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home';
